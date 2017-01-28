ca6a02bc308666be1a4824ffdb65c642917fe830
Michael Weaver <georgemclinly@yahoo.com>
2017-01-28 10:48:48 -0500

    Added Logout Functionality to all pages.

--- InventoryManager/InventoryManager/EmployeeScreenFrom.Designer.cs
+++ InventoryManager/InventoryManager/EmployeeScreenFrom.Designer.cs
@@ -135,13 +135,15 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 37);
             this.button1.TabIndex = 8;
             this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.logoutClick);
             // 
             // EmployeeScreen
             // 
