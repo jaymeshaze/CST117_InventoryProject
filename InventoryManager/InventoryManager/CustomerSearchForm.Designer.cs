36e2d07a4731299baffa1daa8993e89b5e920afe
jaymeshaze <jaymeshaze@outlook.com>
2017-01-29 13:57:50 -0700

    Finished customer GUI elements, tested for errors

--- InventoryManager/InventoryManager/CustomerSearchForm.Designer.cs
+++ InventoryManager/InventoryManager/CustomerSearchForm.Designer.cs
@@ -35,18 +35,30 @@
             this.CustomerSearchAllButton = new System.Windows.Forms.Button();
             this.or_label = new System.Windows.Forms.Label();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
             // 
             // CustomerSearchBox
             // 
             this.CustomerSearchBox.BackColor = System.Drawing.Color.WhiteSmoke;
             this.CustomerSearchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.CustomerSearchBox.Font = new System.Drawing.Font("Neuropol", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CustomerSearchBox.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.CustomerSearchBox.Location = new System.Drawing.Point(715, 332);
-            this.CustomerSearchBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CustomerSearchBox.Location = new System.Drawing.Point(536, 270);
+            this.CustomerSearchBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CustomerSearchBox.Name = "CustomerSearchBox";
-            this.CustomerSearchBox.Size = new System.Drawing.Size(414, 40);
+            this.CustomerSearchBox.Size = new System.Drawing.Size(311, 37);
             this.CustomerSearchBox.TabIndex = 1;
             this.CustomerSearchBox.Text = "Search T2 Games";
             this.CustomerSearchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -60,10 +72,10 @@
             this.CustomerSearchBoxButton.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
             this.CustomerSearchBoxButton.FlatAppearance.BorderSize = 0;
             this.CustomerSearchBoxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CustomerSearchBoxButton.Location = new System.Drawing.Point(1131, 329);
+            this.CustomerSearchBoxButton.Location = new System.Drawing.Point(848, 267);
             this.CustomerSearchBoxButton.Margin = new System.Windows.Forms.Padding(0);
             this.CustomerSearchBoxButton.Name = "CustomerSearchBoxButton";
-            this.CustomerSearchBoxButton.Size = new System.Drawing.Size(55, 46);
+            this.CustomerSearchBoxButton.Size = new System.Drawing.Size(41, 37);
             this.CustomerSearchBoxButton.TabIndex = 3;
             this.CustomerSearchBoxButton.TabStop = false;
             this.CustomerSearchBoxButton.UseVisualStyleBackColor = false;
@@ -72,22 +84,22 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::InventoryManager.Properties.Resources.TTG_LOGO_LOGIN_SCREEN_WIDTH_477PX;
-            this.pictureBox1.Location = new System.Drawing.Point(708, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(531, -2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(648, 395);
+            this.pictureBox1.Size = new System.Drawing.Size(486, 321);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // CustomerSearchAllButton
             // 
             this.CustomerSearchAllButton.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.CustomerSearchAllButton.Font = new System.Drawing.Font("Neuropol", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerSearchAllButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CustomerSearchAllButton.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.CustomerSearchAllButton.Location = new System.Drawing.Point(716, 638);
-            this.CustomerSearchAllButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CustomerSearchAllButton.Location = new System.Drawing.Point(439, 231);
+            this.CustomerSearchAllButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CustomerSearchAllButton.Name = "CustomerSearchAllButton";
-            this.CustomerSearchAllButton.Size = new System.Drawing.Size(471, 132);
+            this.CustomerSearchAllButton.Size = new System.Drawing.Size(353, 107);
             this.CustomerSearchAllButton.TabIndex = 4;
             this.CustomerSearchAllButton.Text = "SEARCH ALL GAMES";
             this.CustomerSearchAllButton.UseVisualStyleBackColor = false;
@@ -98,46 +110,156 @@
             this.or_label.AutoSize = true;
             this.or_label.Font = new System.Drawing.Font("Arial Narrow", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.or_label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.or_label.Location = new System.Drawing.Point(925, 482);
+            this.or_label.Location = new System.Drawing.Point(594, 132);
+            this.or_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.or_label.Name = "or_label";
-            this.or_label.Size = new System.Drawing.Size(52, 49);
+            this.or_label.Size = new System.Drawing.Size(42, 40);
             this.or_label.TabIndex = 5;
             this.or_label.Text = "or";
             // 
             // btnLogout
             // 
-            this.btnLogout.Font = new System.Drawing.Font("Neuropol", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnLogout.Location = new System.Drawing.Point(1756, 11);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnLogout.Location = new System.Drawing.Point(1317, 9);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(134, 28);
+            this.btnLogout.Size = new System.Drawing.Size(100, 23);
             this.btnLogout.TabIndex = 9;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CustomerSearchAllButton);
+            this.panel1.Controls.Add(this.or_label);
+            this.panel1.Location = new System.Drawing.Point(122, 349);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1240, 495);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(106, 346);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1240, 495);
+            this.panel2.TabIndex = 11;
+            this.panel2.UseWaitCursor = true;
+            this.panel2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(439, 231);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(353, 107);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SEARCH ALL GAMES";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(594, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "or";
+            this.label1.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(29, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(834, 461);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(869, 14);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(358, 305);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(271, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(312, 37);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Under Construction";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(889, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 37);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Under Construction";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(984, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 39);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Back to Menu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // CustomerSearch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ClientSize = new System.Drawing.Size(1437, 856);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnLogout);
-            this.Controls.Add(this.or_label);
-            this.Controls.Add(this.CustomerSearchAllButton);
             this.Controls.Add(this.CustomerSearchBoxButton);
             this.Controls.Add(this.CustomerSearchBox);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CustomerSearch";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Team 2 Games Customer Search";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.CustomerSearch_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,5 +273,14 @@
         private System.Windows.Forms.Button CustomerSearchAllButton;
         private System.Windows.Forms.Label or_label;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
     }
 }
